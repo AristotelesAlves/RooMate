7189074d2dfea59acdfbaba0248f8ff8ddc47d10
aristotelesAlves <arystotelys@gmail.com>
2026-01-04 17:43:56 -0300

    FIX: Ajustes nas rotas

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,40 +1,38 @@
-'use cliente'
-
 import { Bolt, House, ListCheck, Receipt, ShoppingBag } from "lucide-react"
 
 export default function NavBar() {
     return (
         <div className="w-full border-t-2 border-zinc-200 bg-white fixed bottom-0 p-2">
             <nav className="flex justify-between w-full p-2">
-                <a className="flex items-center justify-center flex-col relative" href="/rt">
-                    <House className="text-blue-600" />
+                <a className="flex items-center justify-center flex-col relative" href="/inicio">
+                    <House className="'text-blue-600'" />
                     <span className="text-xs text-blue-600">
-                        Início
+                        Início 
                     </span>
                     <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
                 </a>
-                <a className="flex items-center justify-center flex-col relative" href="/rt">
+                <a className="flex items-center justify-center flex-col relative" href="/despesas">
                     <Receipt />
                     <span className="text-xs">
                         Despesas
                     </span>
                     <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
                 </a>
-                <a className="flex items-center justify-center flex-col relative" href="/rt">
+                <a className="flex items-center justify-center flex-col relative" href="/compras">
                     <ShoppingBag />
                     <span className="text-xs">
                         Compras
                     </span>
                     <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
                 </a>
-                <a className="flex items-center justify-center flex-col relative" href="/rt">
+                <a className="flex items-center justify-center flex-col relative" href="/tarefas">
                     <ListCheck />
                     <span className="text-xs">
                         Tarefas
                     </span>
                     <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
                 </a>
-                <a className="flex items-center justify-center flex-col relative" href="/rt">
+                <a className="flex items-center justify-center flex-col relative" href="/ajustes">
                     <Bolt />
                     <span className="text-xs">
                         Ajustes
