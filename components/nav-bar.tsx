146de9fb7797053f49251103de0f0d8dbb52b1a3
aristotelesAlves <arystotelys@gmail.com>
2026-01-04 20:01:55 -0300

    STYLE: Correção de cores

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -13,57 +13,57 @@ export default function NavBar() {
         <div className="w-full border-t-2 border-zinc-200 bg-white p-2">
             <nav className="flex justify-between w-full p-2">
                 <a className="flex items-center justify-center flex-col relative" href="/inicio">
-                    <House className={` ${verifyPathName('inicio') ? 'text-blue-600' : null}`} />
-                    <span className={`text-xs ${verifyPathName('inicio') ? 'text-blue-600' : null}`}>
+                    <House className={` ${verifyPathName('inicio') ? 'text-[#237B99]' : null}`} />
+                    <span className={`text-xs ${verifyPathName('inicio') ? 'text-[#237B99]' : null}`}>
                         Início 
                     </span>
                     {
                         verifyPathName('inicio') && (
-                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
+                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-[#237B99]"></div>
                         )
                     }
                 </a>
                 <a className="flex items-center justify-center flex-col relative" href="/despesas">
-                    <Receipt className={` ${verifyPathName('despesas') ? 'text-blue-600' : null} `} />
-                    <span className={`text-xs ${verifyPathName('despesas') ? 'text-blue-600' : null}`}>
+                    <Receipt className={` ${verifyPathName('despesas') ? 'text-[#237B99]' : null} `} />
+                    <span className={`text-xs ${verifyPathName('despesas') ? 'text-[#237B99]' : null}`}>
                         Despesas
                     </span>
                     {
                         verifyPathName('despesas') && (
-                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
+                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-[#237B99]"></div>
                         )
                     }
                 </a>
                 <a className="flex items-center justify-center flex-col relative" href="/compras">
-                    <ShoppingBag className={` ${verifyPathName('compras') ? 'text-blue-600' : null} `} />
-                    <span className={`text-xs ${verifyPathName('compras') ? 'text-blue-600' : null}`}>
+                    <ShoppingBag className={` ${verifyPathName('compras') ? 'text-[#237B99]' : null} `} />
+                    <span className={`text-xs ${verifyPathName('compras') ? 'text-[#237B99]' : null}`}>
                         Compras
                     </span>
                     {
                         verifyPathName('compras') && (
-                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
+                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-[#237B99]"></div>
                         )
                     }
                 </a>
                 <a className="flex items-center justify-center flex-col relative" href="/tarefas">
-                    <ListCheck className={` ${verifyPathName('tarefas') ? 'text-blue-600' : null} `} />
-                    <span className={`text-xs ${verifyPathName('tarefas') ? 'text-blue-600' : null}`}>
+                    <ListCheck className={` ${verifyPathName('tarefas') ? 'text-[#237B99]' : null} `} />
+                    <span className={`text-xs ${verifyPathName('tarefas') ? 'text-[#237B99]' : null}`}>
                         Tarefas
                     </span>
                     {
                         verifyPathName('tarefas') && (
-                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
+                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-[#237B99]"></div>
                         )
                     }
                 </a>
                 <a className="flex items-center justify-center flex-col relative" href="/ajustes">
-                    <Bolt className={` ${verifyPathName('ajustes') ? 'text-blue-600' : null} `} />
-                    <span className={`text-xs ${verifyPathName('ajustes') ? 'text-blue-600' : null}`}>
+                    <Bolt className={` ${verifyPathName('ajustes') ? 'text-[#237B99]' : null} `} />
+                    <span className={`text-xs ${verifyPathName('ajustes') ? 'text-[#237B99]' : null}`}>
                         Ajustes
                     </span>
                     {
                         verifyPathName('ajustes') && (
-                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-blue-600"></div>
+                            <div className="w-1 h-1 rounded-full -bottom-2 absolute bg-[#237B99]"></div>
                         )
                     }
                 </a>
