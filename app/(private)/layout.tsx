473c735fd2a7e1e7a91ce4ea00435bc45c19f31d
aristotelesAlves <arystotelys@gmail.com>
2026-01-04 19:29:16 -0300

    FEAT: Adicionando ao layout o sonner

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import NavBar from "@/components/nav-bar";
 import Header from "@/components/header";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <div
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col w-full min-h-screen`}
       >
+        <Toaster />
         <Header/>
         <div className="flex-1 p-2 h-full w-full overflow-y-scroll bg-[#F9F7F5]">
           {children}
